7bcfa4ff60e9155b5a6d9bf8450cfdb6046c72f0
Raine <bencol1022@gmail.com>
2026-08-12 16:48:24 -0500

    initial commit

---
+++ src/main/java/net/runicrituals/RunicRitualsClient.java
@@ -0,0 +1,11 @@
+package net.runicrituals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RunicRitualsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/runicrituals/RunicRitualsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.runicrituals;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RunicRitualsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
